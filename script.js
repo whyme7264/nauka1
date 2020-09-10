428be8f42348b277aee246eb4278667d1548801f
whyme7264 <71074245+whyme7264@users.noreply.github.com>
2020-09-10 13:40:59 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+alert('hello');
+const answer = prompt('masz 18?', '18');
+const answers = [];
+answers[0] = prompt('twoje imie','');
+answers[1] = prompt('twoje nazwisko','');
+answers[2] = prompt('twoj wiek','');
+
+document.write('dssvf');
